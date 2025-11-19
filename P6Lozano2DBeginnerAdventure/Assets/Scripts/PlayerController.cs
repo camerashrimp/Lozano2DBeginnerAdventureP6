969acc9c5c6395514067b4421e6929ae5c96dd07
camerashrimp <731406@student.dusd.net>
2025-11-19 13:49:56 -0800

    stepdecor
    added assets and fixing up tile map

--- P6Lozano2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P6Lozano2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    public float speed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
